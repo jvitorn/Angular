ba5b2208a2949ead4d9fa6e73234545989fe4ca4
João Vitor Novais <joaoeuno2011@gmail.com>
2020-05-17 19:29:33 +0000

    criando e implementando component

--- angularApp/src/app/somar/somar.module.ts
+++ angularApp/src/app/somar/somar.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SomarComponent } from './component/somar/somar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SomarComponent],
   imports: [
     CommonModule
+  ],exports:[
+    SomarComponent
   ]
 })
 export class SomarModule { }
